d2dfeeca26f418cec1e9231d3f3db8c7200e70a4
Diogo Henrique de Souza Costa <101272856+dhscosta@users.noreply.github.com>
2023-04-19 00:33:59 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -113,20 +113,6 @@ public class App
                 break;
 
                 case 6:
-<<<<<<< HEAD
-=======
-                    Intercalacao intercalacao = new Intercalacao();
-                    intercalacao.balanceadaComum();
-                    
-                break;
-
-                case 7:
-                    Intercalacao intercalacaoVariavel = new Intercalacao();
-                    intercalacaoVariavel.balanceadaVariavel();
-                break;
-
-                case 8:
->>>>>>> 8fe81e726864274edfdaa9fc9104a66fd3b37095
                 break;
             }
             printInterface();
