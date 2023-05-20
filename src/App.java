2c69aea5f90af976ba5250f890568a74c6cbdbc0
Matheus Sinis <sinismatheus@gmail.com>
2023-05-20 14:58:50 -0300

    algumas alterações

---
+++ src/App.java
@@ -0,0 +1,130 @@
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import com.opencsv.*;
+
+public class App
+{
+
+    public static void printInterface()
+    {
+        System.out.println("-----------INTERFACE-----------");
+        System.out.println("----ATENÇÃO----SELECIONAR A OPÇÃO 1 DESFAZ QUALQUER OPERAÇÃO DO CRUD FEITA ANTES");
+        System.out.println("0 - Parar a execução");
+        System.out.println("1 - Carregar o arquivo com o os dados do csv (create)");
+        System.out.println("2 - Ler um registro do arquivo usando o id (read)");
+        System.out.println("3 - Atualizar um registro de um jogo (update)");
+        System.out.println("4 - Apagar um registro de jogo (delete)");
+        System.out.println("5 - Mostrar todos os registros do arquivo");
+        System.out.println("6 - Compressão LZW");
+        System.out.println("7 - Compressão Huffman");
+    }
+    public static void main(String[] args) throws Exception {
+
+        //abre o csv 
+        CSVReader csv = new CSVReader(new FileReader("games.csv"));
+        Scanner ler = new Scanner(System.in);
+
+        //lê a primeira linha que informa o nome dos valores
+        csv.readNext();
+        String[] line;
+        
+        //cria um arquivo binario
+        RandomAccessFile arq = new RandomAccessFile("gamees.bin", "rw");
+        printInterface();
+
+        Crud crud = new Crud();
+        int x = 0;
+        System.out.print("Insira a opção que deseja executar: ");
+        x = ler.nextInt();
+
+        while(x != 0){
+            switch(x)
+            {
+                case 1:
+                    
+                    while((line = csv.readNext()) != null)
+                    {
+                        crud.create(line);
+                    }
+                    System.out.println("Aperte ENTER para continuar");
+                    ler.nextLine();
+                    ler.nextLine();
+                    
+                    
+                break;
+                
+                case 2:
+                    System.out.println("Insira o id do jogo que será pesquisado: ");
+                    int pesqId = ler.nextInt();
+                    crud.read(pesqId);
+                break;
+
+                case 3:
+                    System.out.println("Insira todos os campos do registro, mesmo os que não forem alterados");
+                    int iden;
+                    do{
+                        System.out.print("Insira o ID do jogo que sofrerá update: ");
+                        iden = ler.nextInt();
+                    }while(crud.read(iden) == false);
+
+                    System.out.print("Insira o título do jogo: ");
+                    ler.nextLine();
+                    String title = ler.nextLine();
+
+                    System.out.print("O jogo funciona em sistemas windows?(true/false): ");
+                    boolean win = ler.nextBoolean();
+                    System.out.print("O jogo funciona em sistemas mac?(true/false): ");
+                    boolean mac = ler.nextBoolean();
+                    System.out.print("O jogo funciona em sistemas linux?(true/false): ");
+                    boolean linux = ler.nextBoolean();
+
+                    System.out.print("Insira a avaliação do jogo: ");
+                    ler.nextLine();
+                    String rating = ler.nextLine();
+
+                    System.out.print("Insira o preço do jogo(use virgula para decimal): ");
+                    float price = ler.nextFloat();
+
+                    System.out.print("Insira a data de lançamento do jogo(yyyy-mm-dd): ");
+                    SimpleDateFormat fDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                    ler.nextLine();
+                    String stringData = ler.nextLine();
+                    Date data = fDateFormat.parse(stringData);
+
+                    crud.update(iden, title, win, mac, linux, rating, price, data);
+                break;
+
+                case 4:
+                    int identificador;
+                    do{
+                        System.out.println("Insira o id do jogo que será deletado: ");
+                        identificador = ler.nextInt();
+                    }while(crud.read(identificador) == false);
+                    crud.delete(identificador);
+                break;
+
+                case 5:
+                    crud.mostrarTodos();
+                    System.out.println("Aperte ENTER para continuar");
+                    ler.nextLine();
+                    ler.nextLine();
+                break;
+
+                case 6:
+                    //código para execução do lzw
+                break;
+
+                case 7:
+                    //código para execução do Huffman
+                break;
+            }
+            printInterface();
+            x = ler.nextInt();
+        }
+
+        csv.close();
+        ler.close();
+        arq.close();
+    }
+}
