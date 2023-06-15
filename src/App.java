92558c1ca3a50f2c8cabbd39696516139b0dd35e
Matheus Sinis <sinismatheus@gmail.com>
2023-06-15 08:32:46 -0300

    criação da branch de criptografia

--- src/App.java
+++ src/App.java
@@ -12,10 +12,8 @@ public class App
         System.out.println("----ATENÇÃO----SELECIONAR A OPÇÃO 1 DESFAZ QUALQUER OPERAÇÃO DO CRUD FEITA ANTES");
         System.out.println("0 - Parar a execução");
         System.out.println("1 - Carregar o arquivo com o os dados do csv (create)");
-        System.out.println("2 - Ler um registro do arquivo usando o id (read)");
-        System.out.println("3 - Atualizar um registro de um jogo (update)");
-        System.out.println("4 - Apagar um registro de jogo (delete)");
-        System.out.println("5 - Mostrar todos os registros do arquivo");
+        System.out.println("2 - Executar criptografia DES");
+        System.out.println("3 - Executar criptografia RSA");
     }
     public static void main(String[] args) throws Exception {
 
@@ -53,60 +51,11 @@ public class App
                 break;
                 
                 case 2:
-                    System.out.println("Insira o id do jogo que será pesquisado: ");
-                    int pesqId = ler.nextInt();
-                    crud.read(pesqId);
+                    //espaço reservado para o código de DES
                 break;
 
                 case 3:
-                    System.out.println("Insira todos os campos do registro, mesmo os que não forem alterados");
-                    int iden;
-                    do{
-                        System.out.print("Insira o ID do jogo que sofrerá update: ");
-                        iden = ler.nextInt();
-                    }while(crud.read(iden) == false);
-
-                    System.out.print("Insira o título do jogo: ");
-                    ler.nextLine();
-                    String title = ler.nextLine();
-
-                    System.out.print("O jogo funciona em sistemas windows?(true/false): ");
-                    boolean win = ler.nextBoolean();
-                    System.out.print("O jogo funciona em sistemas mac?(true/false): ");
-                    boolean mac = ler.nextBoolean();
-                    System.out.print("O jogo funciona em sistemas linux?(true/false): ");
-                    boolean linux = ler.nextBoolean();
-
-                    System.out.print("Insira a avaliação do jogo: ");
-                    ler.nextLine();
-                    String rating = ler.nextLine();
-
-                    System.out.print("Insira o preço do jogo(use virgula para decimal): ");
-                    float price = ler.nextFloat();
-
-                    System.out.print("Insira a data de lançamento do jogo(yyyy-mm-dd): ");
-                    SimpleDateFormat fDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                    ler.nextLine();
-                    String stringData = ler.nextLine();
-                    Date data = fDateFormat.parse(stringData);
-
-                    crud.update(iden, title, win, mac, linux, rating, price, data);
-                break;
-
-                case 4:
-                    int identificador;
-                    do{
-                        System.out.println("Insira o id do jogo que será deletado: ");
-                        identificador = ler.nextInt();
-                    }while(crud.read(identificador) == false);
-                    crud.delete(identificador);
-                break;
-
-                case 5:
-                    crud.mostrarTodos();
-                    System.out.println("Aperte ENTER para continuar");
-                    ler.nextLine();
-                    ler.nextLine();
+                    //espaço reservado para o código de RSA
                 break;
             }
             printInterface();
